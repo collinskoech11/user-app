7d2db94ba2f3d87d0211703aefe6d98453b661fc
Collins Koech <collinskoechck34@gmail.com>
2020-09-14 04:04:15 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React from 'react';
-import './App.css';
 import './script.js';
-import Hello from './sayHello';
+import { Carousel } from 'react-bootstrap';
 
 function App(){
+<<<<<<< HEAD
+=======
   const sayHello = () =>{
-    console.log('sasa');
+    console.log('hello');
   }
+>>>>>>> 564bfeb3e601b22ef827f0fb8265a9c24dce0880
   return(
-    <div>
-      <Hello/>
-    </div>
+    <Carousel>
+    <Carousel.Item interval={1000}>
+      <img
+        className="d-block w-100"
+        src="nature1.jpg"
+        alt="First slide"
+      />
+      <Carousel.Caption>
+        <h3>Sunset</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item interval={500}>
+      <img
+        className="d-block w-100"
+        src="nature2.jpg"
+        alt="Third slide"
+      />
+      <Carousel.Caption>
+        <h3>Green land</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="nature3.jpg"
+        alt="Third slide"
+      />
+      <Carousel.Caption>
+        <h3>Hills</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
   );
 }
 export default App;
